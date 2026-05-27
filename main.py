b379e83055c8a9634624a28b800ccbb8540f34c4
philipp <filipp.2002ivanov@gmail.com>
2026-05-27 22:53:25 +0300

    ad router

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import uvicorn
 from fastapi import FastAPI
+from app.api.v1.users import router as users_router
 
 app = FastAPI()
 
+app.include_router(users_router,
+                   prefix="/users",
+                   tags=["users"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
